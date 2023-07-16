131d3ee791f30820352043d709c400779f6ad203
Sean Ryan <sean.ryan@mendix.com>
2023-07-16 20:27:53 +0200

    Make the webserver multithreaded for perf

--- main_web_service.py
+++ main_web_service.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from random import randint
 from urllib.parse import urlparse, parse_qs
+import time, threading, socket
 
 import config
 import main_service
@@ -163,14 +164,41 @@ class MyServer(BaseHTTPRequestHandler):
     def write(self, content):
         self.wfile.write(bytes(content, "utf-8"))
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((config.HOSTNAME, config.PORT), MyServer)
-    print("Server started http://%s:%s" % (config.HOSTNAME, config.PORT))
-    print("[press any key to stop]")
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
+if __name__ == "__main__":
+    # Multi-threaded server, else performance is terrible
+    # ref https://stackoverflow.com/questions/46210672/python-2-7-streaming-http-server-supporting-multiple-connections-on-one-port
+    #
+    # Create ONE socket.
+    addr = (config.HOSTNAME, config.PORT)
+    sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(addr)
+    sock.listen(5)
 
-    webServer.server_close()
+    # Launch many listener threads.
+    class Thread(threading.Thread):
+        def __init__(self, i):
+            threading.Thread.__init__(self)
+            self.i = i
+            self.daemon = True
+            self.start()
+        def run(self):
+            httpd = HTTPServer(addr, MyServer, False)
+
+            # Prevent the HTTP server from re-binding every handler.
+            # https://stackoverflow.com/questions/46210672/
+            httpd.socket = sock
+            httpd.server_bind = self.server_close = lambda self: None
+
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                pass
+            httpd.server_close()
+
+    print(f"Server started at http://{config.HOSTNAME}:{config.PORT} - {config.WEB_SERVER_THREADS} threads")
+    print("[press any key to stop]")
+    [Thread(i) for i in range(config.WEB_SERVER_THREADS)]
+    input("Press ENTER to kill server")
+
     print("Server stopped.")
